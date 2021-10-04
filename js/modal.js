384a67fad08695ea7097c745a2ab45eae83ab772
guna_9 <guna_9@naver.com>
2021-10-04 23:54:18 +0900

    footer lng&cur modal

--- js/modal.js
+++ js/modal.js
@@ -18,6 +18,7 @@ mypageBtn.addEventListener("click", () => {
 });
 //마이페이지 모달
 
+
 const lngCurBtn = document.querySelector(".nav__right-global");
 const lngCurModal = document.querySelector(".lng-cur__modal");
 const lngCurClose = lngCurModal.querySelector(".btn-close");
@@ -30,3 +31,32 @@ lngCurClose.addEventListener("click", () => {
     lngCurModal.classList.remove('active');
     modalActiveBg.classList.remove('active');
 })
+// header-언어&통화 모달
+
+
+const footerLngModal = document.querySelector(".language__modal");
+const footerLngBtn = document.querySelector(".fnb__bottom-language");
+
+const footerCurModal = document.querySelector(".currency__modal");
+const footerCurBtn = document.querySelector(".fnb__bottom-currency");
+
+const lngClose = footerLngModal.querySelector(".btn-close");
+const curClose = footerCurModal.querySelector(".btn-close");
+
+footerLngBtn.addEventListener("click", () => {
+    footerLngModal.classList.add('active');
+    modalActiveBg.classList.add('active');
+})
+lngClose.addEventListener("click", () => {
+    footerLngModal.classList.remove('active');
+    modalActiveBg.classList.remove('active');
+})
+footerCurBtn.addEventListener("click", () => {
+    footerCurModal.classList.add('active');
+    modalActiveBg.classList.add('active');
+})
+curClose.addEventListener("click", () => {
+    footerCurModal.classList.remove('active');
+    modalActiveBg.classList.remove('active');
+})
+// footer-언어, 통화 모달

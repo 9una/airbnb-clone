ed5a0977cb40f61f812f1bd4195ecdc1c7e26106
9una <guna_9@naver.com>
2021-11-04 16:21:52 +0900

    search modal css

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,5 @@
+const modalBg = document.getElementById('modalBg');
+
 //마이페이지 모달
 const mypageModal = document.querySelector(".nav__right-myBtn-modal");
 
@@ -6,52 +8,41 @@ function mypageModalToggle(){
     mypageModal.classList.toggle('active');
 }
 
-// header-언어&통화 모달
-const modalActiveBg = document.getElementById("modalActiveBg"),
-    lngCurBtn = document.querySelector(".nav__right-global"),
+// 언어&통화 모달
+const lngCurBtn = document.querySelector(".nav__right-global"),
     lngCurModal = document.querySelector(".lng-cur__modal");
 
 function gnbModalLngCur(){
     lngCurModal.classList.add('active');
-    modalActiveBg.classList.add('active');
+    modalBg.classList.add('active');
 }
 function gnbModalClose(){
     lngCurModal.classList.remove('active');
-    modalActiveBg.classList.remove('active');
+    modalBg.classList.remove('active');
 }
 
-// footer-언어&통화 모달
-const footerLngBtn = document.querySelector(".fnb__bottom-language"),
-    footerCurBtn = document.querySelector(".fnb__bottom-currency"),
-    fnbLngModal = document.querySelector(".fnb__modal-lng"),
-    fnbCurModal = document.querySelector(".fnb__modal-cur");
+// 언어&통화 모달 탭전환
+const typeLngCurBtn = document.querySelectorAll('.lng-cur__type button'),
+    typeLngCur = document.querySelectorAll('.lng-cur__cont-box > div');
 
-function fnbModalLng(){
-    fnbLngModal.classList.add('active');
-    modalActiveBg.classList.add('active');
-}
-function fnbModalLngClose(){
-    fnbLngModal.classList.remove('active');
-    modalActiveBg.classList.remove('active');
+function tabLngCur(){
+    console.log('탭!');
 }
 
-function fnbModalCur(){
-    fnbCurModal.classList.add('active');
-    modalActiveBg.classList.add('active');
-}
-function fnbModalCurClose(){
-    fnbCurModal.classList.remove('active');
-    modalActiveBg.classList.remove('active');
-}
+/*
+1 - 첫번째 버튼은 active 상태 유지
+3 - 버튼 두개와 컨텐츠 2개의 index 연결
+2 - 두번쨰 버튼을 누르면 1번의 active 없어짐
+*/
 
 // header - searchForm 모달
+const searchForm = document.querySelector('.nav__form-search');
 const searchTab = document.querySelectorAll('.form-search__form.active > div');
-const modalTab = document.querySelectorAll('.boxbundle > div');
 const searchBtn = document.querySelector('.nav__form-search > button');
 
 for (let i = 0; i < searchTab.length; i++) {
-    
     searchTab[i].onclick = function(){
+        searchForm.classList.add('active');
         let j = 0;
         while(j < searchTab.length){
             searchTab[j++].className = "modal-tab";
@@ -59,8 +50,5 @@ for (let i = 0; i < searchTab.length; i++) {
         searchTab[i].className = 'modal-tab active';
         searchBtn.classList.add('active');
     }
-        
 }
-
-
 //https://kyounghwan01.github.io/blog/JS/JSbasic/addEventListener/ 참고
